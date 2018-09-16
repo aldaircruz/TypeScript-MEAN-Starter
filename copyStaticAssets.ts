6635d6922aae7728f2489333624e889ca44c7d4d
Aldair Cruz <aldaircruz@users.noreply.github.com>
2018-09-16 20:54:59 +0100

    chore(devDependency): remove shelljs

--- copyStaticAssets.ts
+++
@@ -1,8 +0,0 @@
-import * as shell from "shelljs";
-
-const baseDevDirName = "server";
-const distDirName = "dist";
-
-shell.cp("-R", `${baseDevDirName}/public/js/lib`, `${distDirName}/public/js/`);
-shell.cp("-R", `${baseDevDirName}/public/fonts`, `${distDirName}/public/`);
-shell.cp("-R", `${baseDevDirName}/public/images`, `${distDirName}/public/`);
