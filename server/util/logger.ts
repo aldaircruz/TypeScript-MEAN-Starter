57e0472e7595aebae00a9dbadf24f01d6625174c
Aldair Cruz <aldaircruz@users.noreply.github.com>
2018-09-16 10:53:12 +0100

    merge 'Microsoft/TypeScript-Node-Starter' and 'ngx-rocket/generator-ngx-rocket' generated project config files

--- server/util/logger.ts
+++ server/util/logger.ts
@@ -1,5 +1,5 @@
-import winston from "winston";
-import { Logger } from "winston";
+ import winston from "winston";
+ import { Logger } from "winston";
 import { ENVIRONMENT } from "./secrets";
 
 const logger = new (Logger)({
