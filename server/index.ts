64453bbd40cecf030fc1d8250c200aced8e4d281
Aldair Cruz <aldaircruz@users.noreply.github.com>
2018-10-02 23:41:50 +0100

    chore(*): cleanu-up angular universal configs

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,7 @@
 import 'reflect-metadata';
-import { enableProdMode } from '@angular/core';
 
 import * as express from 'express';
 import { join } from 'path';
-
-// Faster server renders w/ Prod mode (dev mode never needed)
-enableProdMode();
 
 const PORT = process.env.PORT || 4000;
 const INDEX = join(process.cwd(), 'dist', 'public');
